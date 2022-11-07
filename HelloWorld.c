5201c666156c1f42e94f69e59611c0376a84dcd8
Priyansu Mukherjee <84275293+root311Joker@users.noreply.github.com>
2022-11-07 23:43:21 +0530

    Add files via upload

---
+++ HelloWorld.c
@@ -0,0 +1,15 @@
+//                          N O T E      THIS    P R O P E R L Y                          //
+// STDIO.h >> we use stdio. h in C is that this header file imports different variables, macros, 
+//and functions to perform input and output operations.
+
+// int main >> represents that the function returns some integer even '0' at the end of the program execution.
+// printf >> To display the output to the user
+//return 0 means that the program will execute successfully and did what it was intended to do.
+
+//WAP to print hello world
+#include<stdio.h>
+int main()
+{
+    printf("HELLO WORLD");
+    return 0;
+}
